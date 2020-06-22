27fb375bf84133bfa4d3d16faa6cbd0b4fe02ceb
汪心禾 <wangxinhe06@gmail.com>
2020-06-22 18:14:17 +0800

    Create setup.py

--- Makefile
+++ Makefile
@@ -1,3 +1,42 @@
+build: clean-build
+	python3 setup.py build
+
+dist: clean
+	python3 setup.py sdist bdist_wheel
+
+install: build
+	python3 setup.py install
+
+compileall: clean-pycache
+	python3 -m compileall geodn
+
+
+clean: clean-build clean-dist clean-egg-info
+
+clean-pycache:
+	rm -rf geodn/__pycache__
+
+clean-build:
+	rm -rf build
+
+clean-dist:
+	rm -rf dist
+
+clean-egg-info:
+	rm -rf geodn.egg-info
+
+
+pip:
+	pip install -Ur requirements.txt
+
+
+test: 
+	python3 test_geodn.py
+
+pytest:
+	pytest --cov=geodn
+
+
 lint: flake8 pylint
 
 flake8:
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='geodn',
+    version='0.0.20200616',
+    author='汪心禾',
+    author_email='wangxinhe06@gmail.com',
+    description='Geo Domain Name',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wxh06/geodn',
+    packages=setuptools.find_packages(),
+    package_data={'': ['GeoLite2*/*.mmdb']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
